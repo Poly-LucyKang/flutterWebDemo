1acc9345812561cdcba699f01acb2186c3da5a72
poly-lucykang <lucy.kang@edu-poly.com>
2024-11-29 10:20:41 +0900

    Update Flutter Web app

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3531208525"
+    serviceWorkerVersion: "2704642801"
   }
 });
